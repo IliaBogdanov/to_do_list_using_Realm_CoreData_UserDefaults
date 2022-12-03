afe637484df93b46c78f8f57a654afb6e658392e
IliaBogdanov <bogdanov.ilua@gmail.com>
2022-12-03 23:09:51 +0300

    table view for Realm list is added

---
+++ Homework_14/RealmTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RealmTableViewCell.swift
+//  Homework_14
+//
+//  Created by Илья Богданов on 03.12.2022.
+//
+
+import UIKit
+
+class RealmTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
